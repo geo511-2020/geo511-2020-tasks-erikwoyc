3a40066e77114d3b848bf249ddadb128b274e9e8
erikwoyc <erikwoyc@buffalo.edu>
2020-09-29 14:05:53 -0400

    1st Commit

---
+++ week_04/Case_Study4.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(nycflights13)
+library(dplyr)
+# Exploring Data
+str(airports)
+view(airports)
+str(flights)
+view(flights)
+## Table Join
+Airport_join <- left_join(flights, airports, by = c("dest" = "faa"))
+##Farthest Airport
+Farthest_Airport <- Airport_join %>%
+  arrange(desc(distance)) %>%
+  slice(1)
+view(Farthest_Airport)
+## Answer
+## Honolulu Intl
