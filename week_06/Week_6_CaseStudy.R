4e2d44d92d57ab6a5e5a707a34e963d42bbfd546
erikwoyc <erikwoyc@buffalo.edu>
2020-10-13 15:31:57 -0400

    1st commit

---
+++ week_06/Week_6_CaseStudy.R
@@ -0,0 +1,36 @@
+library(raster)
+library(sp)
+library(spData)
+library(tidyverse)
+library(sf)
+library(ncdf4)
+# Data Download
+download.file("https://crudata.uea.ac.uk/cru/data/temperature/absolute.nc","absolute.nc")
+tmean=raster("absolute.nc")
+plot(tmean)
+# World Filter
+data(world)  #load 'world' data from spData package
+data_poly <- world %>% 
+  filter(continent != 'Antarctica')
+sp_world <- as(data_poly, 'Spatial')
+plot(sp_world)
+
+#Prep Climate Data
+TempC <- raster::extract(tmean, sp_world, fun = max, na.rm = 1, 
+                         smal = 1, sp = 1)
+TempC_sf <- st_as_sf(TempC)
+TempC_sf
+# Plot
+ggplot() + geom_sf(data = TempC_sf, 
+                   aes(fill = TempC_sf$CRU_Global_1961.1990_Mean_Monthly_Surface_Temperature_Climatology)) +
+  scale_fill_viridis_c(name="Annual\nMaximum\nTemperature (C)") + 
+  theme(legend.position = 'bottom')
+# Communicate the Results
+library(dplyr)
+#view(TempC_sf)
+#Results <- TempC_sf %>% group_by(continent) %>%......(still working this part out)
+  
+  
+
+                   
+                   
